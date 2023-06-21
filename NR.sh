d350f438e0915f4d6b290b691c0010349b7cb2d4
MtheEPIC <mtheepic1423@protonmail.com>
2023-06-21 19:35:45 -0400

    V1.1.0 - hostname changer and nice prompts

--- NR.sh
+++ NR.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
 # Define the programs to check and install
-declare -gra programs=("geoip-bin" "tor" "sshpass" "nipe")
-declare -gra rm_programs=("whois" "nmap" "geoip-bin") # "curl") #"nipe")
-# Get the username using 'logname' command
-declare -gr username=$(logname)
+declare -rga programs=("cowsay" "geoip-bin" "tor" "sshpass" "nipe")
+declare -rga rm_programs=("whois" "nmap" "geoip-bin") # "curl") #"nipe")
 # Regular expression patterns for IP address and domain
 declare -rg ip_pattern='^(25[0-5]|2[0-4][0-9]|[1-9][0-9]?|0)(\.(25[0-5]|2[0-4][0-9]|[1-9][0-9]?|0)){3}$'
 declare -rg domain_pattern='^([a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9]\.)+[a-zA-Z]{2,}$'
 # Define path for the nipe program installation
 #nipe_path="/home/$username/nipe"
+declare -rg HNAME_PATH="/etc/hostname"
+declare -rg HNAME_BAK="$script_dir/hostname.bak"
 declare -rg nipe_path="/usr/bin/nipe"
 declare -rg nipepl_path="/usr/bin/nipe/nipe.pl"
+declare -rg COWSAY_PATH="/usr/games/cowsay"
+declare -rg COWTHINK_PATH="/usr/games/cowthink"
 # remote connection type
 declare -gi rm_mode="1" # 0-localhost 1-in lan 2-public 3-hidden service
 # values for SSHPASS
 rm_ip="10.0.0.70"
 rm_user="michael"
 rm_pass="michael"
+rm_port="22"
 # Get the directory path of the bash script
 script_dir="$(dirname "$0")"
 [[ $script_dir == "." ]] && script_dir=$(pwd)
@@ -50,7 +53,7 @@ init_checks() {
 	[ ! -d $SCAN_PATH ] && mkdir $SCAN_PATH
 	[ ! -f $LOG_PATH ] && sudo touch $LOG_PATH
 	
-	while getopts ":hrm:" opt; do
+	while getopts ":hrm:" opt; do # TODO more falgs (for sshapss creds)
 		case $opt in
 			h)
 				usage
@@ -134,11 +137,21 @@ install_programs() {
 }
  
 revert_to_default() {
+	[ -f "$HNAME_BAK" ] && cat $HNAME_BAK > $HNAME_PATH
+
 	[ ! -d "$nipe_path" ] && echo "[!] Can't stop the service that isn't installed" && return 1
 
 	sudo perl $nipe_path/nipe.pl stop >/dev/null 2>&1
 	echo "[*] nipe is disabled"
 	return 0
+}
+
+say() {
+	figlet $1 -cf slant | $COWSAY_PATH -n
+}
+
+think() {
+	figlet $1 -k | $COWTHINK_PATH -n
 }
 
 check_domain_format() {
@@ -157,7 +170,10 @@ check_domain_format() {
 	fi
 }
 
-spoof_address() {
+spoof_address() {	
+	[ ! -f "$HNAME_BAK" ] && cp $HNAME_PATH $HNAME_BAK
+	echo $RANDOM > HNAME_PATH
+
 	cd "$nipe_path"
 	nipe_status=$(sudo perl nipe.pl status | grep -oP "(?<=Status: )\b(true|false)\b")
 	[[ $nipe_status == "true" ]] && return 0 #echo "[#] nipe is already running." && return 0
@@ -169,6 +185,9 @@ spoof_address() {
 	[[ $nipe_status == "true" ]] && return 0
 
 	echo "[!] Unexpected Error while starting nipe service"
+
+	think "mmm... maybe try again..."
+
 	sudo perl nipe.pl stop
 	exit 1
 }
@@ -186,27 +205,21 @@ get_spoofed_value() {
 
 remote_scan() {
 	local rm_scans="/home/michael" #TODO change to dynamic
-
-
-	local array_string=$(printf "%s " "${rm_programs[@]}")
-	sshpass -p $rm_pass ssh -o StrictHostKeyChecking=no $rm_user@$rm_ip "$(declare -f remote_script); $(declare -f install_programs); $(declare -f check_installed); remote_script $target_domain $array_string" 
-		
+	local rm_port=22
 	local whois_scan="whois_$target_domain"
 	local nmap_scan="nmap_$target_domain"
-	
+	local array_string=$(printf "%s " "${rm_programs[@]}")
 
-	# get_whois $target_domain
+	sshpass -p $rm_pass ssh -o StrictHostKeyChecking=no $rm_user@$rm_ip "$(declare -f remote_script); $(declare -f install_programs); $(declare -f check_installed); remote_script $target_domain $array_string" 
 
-
-	sshpass -p $rm_pass ssh -o StrictHostKeyChecking=no $rm_user@$rm_ip "$(declare -f get_whois); get_whois $target_domain" "$rm_pass"
-	sudo sshpass -p $rm_pass scp $rm_user@$rm_ip:$rm_scans/$whois_scan $SCAN_PATH
+	echo -e "\n[*] Whoising victim's address:"
+	sshpass -p $rm_pass ssh -p $rm_port -o StrictHostKeyChecking=no $rm_user@$rm_ip whois $target_domain > $SCAN_PATH/$whois_scan
 	echo "[@] Whois data was saved into $SCAN_PATH/$whois_scan."
 	# Log Audit 
 	echo "$(date)- [*] whois data collected for: $target_domain" >> $LOG_PATH
 	
-	# get_nmap $target_domain
-	sshpass -p $rm_pass ssh -o StrictHostKeyChecking=no $rm_user@$rm_ip "$(declare -f get_nmap); get_nmap $target_domain" "$rm_pass"
-	sudo sshpass -p $rm_pass scp $rm_user@$rm_ip:/var/log/NR/$nmap_scan $SCAN_PATH
+	echo -e "\n[*] Scanning victims's address:"
+	sshpass -p $rm_pass ssh -p $rm_port -o StrictHostKeyChecking=no $rm_user@$rm_ip nmap -vvv -T4 $target_domain > $SCAN_PATH/$nmap_scan
 	echo "[@] Nmap data was saved into $SCAN_PATH/$nmap_scan."
 	# Log Audit
 	echo "$(date)- [*] Nmap data collected for: $target_domain" >> $LOG_PATH
@@ -215,7 +228,6 @@ remote_scan() {
 #####################REMOTE-SCRIPTS####################
 
 remote_script() {
-	# local log_dir="/var/log/NR"
 	local programs=("whois" "nmap" "geoip-bin" "git" "curl" "build-essential" "nipe")
 	local sudo_pass="michael"
 	local target=$1
@@ -227,9 +239,10 @@ remote_script() {
 	install_programs ${programs[@]} >/dev/null 2>&1
 
 	sudo perl $nipe_path/nipe.pl start
-	local status=$(sudo perl $nipe_path/nipe.pl status)
-	local remote_ip=$(echo $status | awk '/Ip:/ { print $NF } ')
+	local status=$(sudo perl $nipe_path/nipe.pl status) # TODO handel a failed status
+	
 
+	local remote_ip=$(echo $status | awk '/Ip:/ { print $NF } ')
 	local country_ip=$(geoiplookup $remote_ip | cut -d " " -f 5-)
 
 	echo "Uptime:$(uptime)"
@@ -242,49 +255,40 @@ get_whois() {
 	local rm_pass=$2
 	local scan_path="whois_$target"
 
-	echo -e "\n[*] Whoising victim's address:"
 	pwd
 	echo "$rm_pass" | sudo -S touch $scan_path
 	sudo chmod a+w $scan_path
 	whois $target > $scan_path	
 }
 
-get_nmap() {
-	local target=$1
-	local rm_pass=$2
-	local scan_path="nmap_$target"
-	
-	echo -e "\n[*] Scanning victims's address:"
-	echo "$rm_pass" | sudo -S touch $scan_path
-	# -A = -O -sV -sC --traceroute
-	sudo nmap -vvv -T4 $target -oN $scan_path >/dev/null 2>&1
-}
-
 #########################################################
 
-init_checks $@
+main() {
+	init_checks $@
+	
+	install_programs ${programs[@]} </dev/null 2>&1
+	
+	spoof_address
+	get_spoofed_value
+	
+	declare -g target_domain=""
+	while [ -z "$target_domain" ]
+	do
+		check_domain_format
+	done
+	
+	echo -e "\n[*] Connecting to Remote Server:"
+	
+	start_date=$(date +%s)
+	# remote_scan
+	(( $(date +%s) - $start_date > 30 )) && think "well... that was a long wait"
+	
+	say "Have a good day"
+	
+	revert_to_default
+}
 
-install_programs ${programs[@]} </dev/null 2>&1
-
-# figlet "DONE"
-# pwd
-# dirname $0
-# echo $0
-# exit 0
-
-spoof_address
-get_spoofed_value
-
-declare -g target_domain=""
-while [ -z "$target_domain" ]
-do
-	check_domain_format
-done
-
-echo -e "\n[*] Connecting to Remote Server:"
-
-remote_scan
-
+main "{@}"
 
 # get target for remote
 # nmap -p 22 --exclude 10.0.0.50 10.0.0.50/24 | grep -B 4 "open" |  awk '/Nmap scan/{print $NF}' | head -n 1
