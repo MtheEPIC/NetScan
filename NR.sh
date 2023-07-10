a895cb20586673c11a5239c13ba40108381a85af
MtheEPIC <mtheepic1423@protonmail.com>
2023-07-10 17:29:42 -0400

    better visuals

--- NR.sh
+++ NR.sh
@@ -1,38 +1,120 @@
 #!/bin/bash
 
+################################################################################
+#                                                                              #
+# NR.sh																	       #
+#                                                                              #
+# version: 1.1.0                                                               #
+#                                                                              #
+# Network Research - Remote target scanning using tor and a remote server      #
+#																			   #
+# Srudent Name - Michael Ivlev												   #
+# Student Code - S11														   #
+# Class Code - HMagen773616													   #
+# Lectures Name - Eliran Berkovich											   #
+#																			   #
+# GNU GENERAL PUBLIC LICENSE                                                   #
+#                                                                              #
+# This program is free software: you can redistribute it and/or modify         #
+# it under the terms of the GNU General Public License as published by         #
+# the Free Software Foundation, either version 3 of the License, or            #
+# (at your option) any later version.                                          #
+#                                                                              #
+# This program is distributed in the hope that it will be useful,              #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+# GNU General Public License for more details.                                 #
+#                                                                              #
+# You should have received a copy of the GNU General Public License            #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
+#                                                                              #
+################################################################################
+
 # Define the programs to check and install
 declare -rga programs=("cowsay" "geoip-bin" "tor" "sshpass" "nipe")
 declare -rga rm_programs=("whois" "nmap" "geoip-bin") # "curl") #"nipe")
 # Regular expression patterns for IP address and domain
-declare -rg ip_pattern='^(25[0-5]|2[0-4][0-9]|[1-9][0-9]?|0)(\.(25[0-5]|2[0-4][0-9]|[1-9][0-9]?|0)){3}$'
-declare -rg domain_pattern='^([a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9]\.)+[a-zA-Z]{2,}$'
+declare -rg IP_PATTERN='^(25[0-5]|2[0-4][0-9]|[1-9][0-9]?|0)(\.(25[0-5]|2[0-4][0-9]|[1-9][0-9]?|0)){3}$'
+declare -rg DOMAIN_PATTERN='^([a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9]\.)+[a-zA-Z]{2,}$'
 # Define path for the nipe program installation
-#nipe_path="/home/$username/nipe"
+#NIPE_PATH="/home/$username/nipe"
 # Get the directory path of the bash script
-script_dir="$(dirname "$0")"
+script_dir="$(dirname '$0')"
 declare -rg HNAME_PATH="/etc/hostname"
 declare -rg HNAME_BAK="$script_dir/hostname.bak"
-declare -rg nipe_path="/usr/bin/nipe"
+declare -rg NIPE_PATH="/usr/bin/nipe"
 declare -rg nipepl_path="/usr/bin/nipe/nipe.pl"
 declare -rg COWSAY_PATH="/usr/games/cowsay"
 declare -rg COWTHINK_PATH="/usr/games/cowthink"
 # remote connection type
 declare -gi rm_mode="1" # 0-localhost 1-in lan 2-public 3-hidden service
 # values for SSHPASS
-rm_ip="10.0.0.70"
-rm_user="michael"
-rm_pass="michael"
-rm_port="22"
+declare -g rm_ip
+declare -g rm_user
+declare -g rm_pass
+declare -g rm_port
 [[ $script_dir == "." ]] && script_dir=$(pwd)
 # path to the scripts log file
 declare -rg LOG_PATH="/var/log/nr.log"
 # path to saved scans
 declare -rg SCAN_PATH="$(pwd)/scans"
 
+is_cow_time=true
 
-# # Construct the absolute path to ssh key
-#ssh_key_path="$script_dir/id_rsa"
+# Function to cycle through a pattern and make a wave with a given word
+# Parameters:
+#	word to make a wave to it
+cycle_word_and_chars() {
+	local word=$1 #"loading..."
+	local chars="-\|/"
+	
+	local j=0
+	while true; do
+		for (( i=0; i< ${#word}; i++ )); do
+			if [[ ! ${word:i:1} =~ [[:alpha:]] ]]; then continue; fi
+				
+			curr_word="${word:0:i}$(echo ${word:i:1} | tr '[:lower:]' '[:upper:]')${word:i+1}"
+			echo -ne "\r$curr_word${chars:j:1}"
+			(( j++ ))
+			(( $j < ${#chars} )) || j=0
+			(($i + 1 < ${#word} )) && sleep .1
+		done
 
+		read -n 1 -t .1 -s && exit 0
+	done
+}
+
+# Function to make a wave with a given word
+# Parameters:
+#	word to make a wave to it
+cycle_word() {
+	local word="loading"
+	while true; do
+		for (( i=0; i< ${#word}; i++ )); do
+			curr_word="${word:0:i}$(echo ${word:i:1} | tr '[:lower:]' '[:upper:]')${word:i+1}"
+			printf "%s\r" "$curr_word"
+			sleep .1
+		done
+	
+		read -n 1 -t .1 -s && exit 0
+	done
+}
+
+# Function to cycle through a pattern 
+cycle_char() {
+	local chars="-\|/"
+	local word=$1
+	while true; do
+		for (( i=0; i< ${#chars}; i++ )); do
+			echo -ne "\r$word${chars:i:1}"
+			sleep .1
+		done
+	
+		read -n 1 -t .1 -s && exit 0
+	done
+}
+
+# Function to display the correct way to run the script
 usage() {
 	local script_name=$(basename "$0")
 cat << EOF
@@ -46,14 +128,18 @@ Usage: $script_name [Options] {target_domain}
 	3 hidden service
  
 EOF
+
+cycle_word_and_chars "loading..."
+
 }
 
+# Function to check the init condition
 init_checks() {
 	[ $UID -ne 0 ] && echo "[!] This script requires root privileges. Please run with sudo." && exit 1
 	[ ! -d $SCAN_PATH ] && mkdir $SCAN_PATH
 	[ ! -f $LOG_PATH ] && sudo touch $LOG_PATH
 	
-	while getopts ":hrm:" opt; do # TODO more falgs (for sshapss creds)
+	while getopts ":hrm:" opt; do # TODO more flags (for sshapss creds)
 		case $opt in
 			h)
 				usage
@@ -82,7 +168,7 @@ init_checks() {
 	shift $((OPTIND - 1))
 }
 
-# check for internet connectivity without getting blocked
+# Function to check for internet connectivity without getting blocked
 check_connectivity() {
 	return 0
 	#TODO remove the top return
@@ -90,156 +176,230 @@ check_connectivity() {
 	echo "[!] No internet connection available!" && exit 1
 }
 
+# Function to check if an app is already installed
+# Parameters:
+#	$1: app name to check
 check_installed() {
-	np="nipe"
+	local np="nipe"
 	if [ "$1" == "$np" ]; then
 		[ -f "$nipepl_path" ] && echo "[#] nipe is already installed." && return 0 || return 1
 	else
 		#if command -v "$1" >/dev/null 2>&1; then 
 		if dpkg-query -W -f='${Status}' "$1" 2>/dev/null | grep -q "ok installed"; then
-			echo "[#] $1 is already installed."
-			return 0  # Program is already installed
+			tee_audit "[#] $1 is already installed."
+			return 0  
 		else
-			return 1  # Program is not installed
+			audit "[#] $1 isn't installed."
+			return 1  
 		fi
 	fi
 }
 
-# Loop through the programs array and check/install each program
+# Function to Loop through the programs array and check/install each program
+# Paramets:
+#	$1: array of function to install
 install_programs() {
 	local array=("$@")
 
 	for program in "${array[@]}"; do
 		# Skip installation if program is already installed
 		check_installed "$program" && continue 
-		
-		# check_connectivity #TODO make run once
-		
-		echo "[*] Installing $program..."
+			
+		cycle_word_and_chars "[*] Installing $program..." &
+		local load_msg_pid=$!
 		if [ $program == "nipe" ]; then
 			# Install libs and dependencies
-			! command -v cpanm >/dev/null && curl -L https://cpanmin.us | sudo perl - App::cpanminus
+			! command -v cpanm && curl -L https://cpanmin.us | sudo perl - App::cpanminus
 
 			# Download
-			[ -z "$nipe_path" ] && nipe_path="$(pwd)/nipe"
-			sudo git clone https://github.com/htrgouvea/nipe $nipe_path
-			cd $nipe_path # Must cd for the script to find modules
+			[ -z "$NIPE_PATH" ] && NIPE_PATH="$(pwd)/nipe"
+			sudo git clone https://github.com/htrgouvea/nipe $NIPE_PATH >/dev/null 2>&1
+			cd $NIPE_PATH 
 
-			
-			sudo cpanm --installdeps .
-			# sudo cpanm install Nipe::Engine::Stop
-			sudo perl nipe.pl install
+			sudo cpanm --installdeps . >/dev/null 2>&1
+			sudo perl nipe.pl install >/dev/null 2>&1
 		else
-			sudo apt-get update #TODO RUN ONCE
-			sudo apt-get install -y "$program"
+			sudo apt-get update >/dev/null 2>&1 #TODO RUN ONCE
+			sudo apt-get install -y "$program" >/dev/null 2>&1
 		fi
+		kill $load_msg_pid
+		echo -e "\r[*] Installing $program... "
+		audit "[*] $program has been installed"
 	done
 }
- 
+
+# Function to revert spoofed settings to default
 revert_to_default() {
 	[ -f "$HNAME_BAK" ] && cat $HNAME_BAK > $HNAME_PATH && rm $HNAME_BAK
 
-	[ ! -d "$nipe_path" ] && echo "[!] Can't stop the service that isn't installed" && return 1
+	[ ! -d "$NIPE_PATH" ] && echo "[!] Can't stop the service that isn't installed" && return 1
 
-	sudo perl $nipe_path/nipe.pl stop >/dev/null 2>&1
+	sudo perl $NIPE_PATH/nipe.pl stop >/dev/null 2>&1
 	echo "[*] nipe is disabled"
 	return 0
 }
 
+# Function to create a new audit
+audit() {
+	echo "$(date)- $1" >> $LOG_PATH
+}
+
+# Function to create a new audit and display to the std
+tee_audit() {
+	echo $1
+	audit "$1"
+}
+
+# Function wrapper to use a fancy echo when a flag is used
 say() {
-	figlet $1 -cf slant | $COWSAY_PATH -n
+	$is_cow_time && (figlet $1 -cf slant | $COWSAY_PATH -n) || echo $1
 }
 
+# Function wrapper to use a fancy echo when a flag is used
 think() {
-	figlet $1 -k | $COWTHINK_PATH -n
+	$is_cow_time && (figlet $1 -k | $COWTHINK_PATH -n) || echo $1
 }
 
+# Function to check the validity of the given target address
+# Parameters:
+#	$1: The given target address
+# Return:
+#	0 if the input is valid
+#	1 if the input is invalid
 check_domain_format() {
-	read -p "[?] Specify a Domain/IP address to scan: " user_input
-
-	if [[ $user_input =~ $ip_pattern ]]; then
-		# target_domain=$(host "$user_input" | awk '{print $NF}'| sed 's/\.$//')
-		target_domain=$user_input
-		return 0
-	elif [[ $user_input =~ $domain_pattern ]]; then
-		target_domain=$user_input
-		return 0
-	else
-		echo "Invalid input. Please enter a valid IP address or domain."
-		return 1
-	fi
+	local user_input=$1
+	[[ $user_input =~ $IP_PATTERN || $user_input =~ $DOMAIN_PATTERN ]] && echo "$user_input" && return 0 || return 1
 }
 
-spoof_address() {	
+# Function to get a target address from the user
+get_target() {
+	declare -g target_domain=""
+	while true;
+	do
+		read -p "[?] Specify a Domain/IP address to scan: " target_domain
+		check_domain_format "$target_domain"
+		[ $? -eq 0 ] && return 0 || echo "Invalid input. Please enter a valid IP address or domain."
+	done
+}
+
+# Function to reset the tor circut inorder to fix any issues caused by the current setup
+reset_circuit() {
+	cd $NIPE_PATH
+	sudo perl nipe.pl stop
+	
+	echo "Changing tor circuit"
+	
+	local tor_pid=$(sudo netstat -tulep | awk '/:9050/ {print $NF}' | cut -d '/' -f 1)
+	[ -n "$tor_pid" ] && sudo kill $tor_pid
+	
+	tor --controlport 9052 & #TODO sed auth hash
+	local pid=$!
+	sleep 7
+	sudo kill $pid
+
+	sudo perl nipe.pl start
+}
+
+# Function to spoof the local identifiers
+# Note:
+#	The function should spoof IP, Hostname, MAC
+#	currently the MAC spoofing isn't implemented
+spoof_address() {
+	local msg="[*] Spoofing local identifiers..."
+	cycle_word_and_chars "$msg" &
+	local load_msg_pid=$!
+
+	# cat /usr/share/wireshark/manuf
+
+	local mac=$(printf "%012x" $(shuf -i 0-$((16 ** 12 - 1)) -n 1) | sed 's/../&:/g; s/:$//')
+	local adapter=$(route -n | awk 'NR>2 {print $2" "$NF}' | awk '!/0.0.0.0/ {print $NF}') 
+
+	# echo "$adapter: $mac"
+
+	# sudo ip link set dev $adapter down
+	# sudo ip link set dev $adapter address $mac
+	# sudo ip link set dev $adapter up
+
+
 	[ ! -f "$HNAME_BAK" ] && cp $HNAME_PATH $HNAME_BAK
 	echo $RANDOM > $HNAME_PATH
 
-	cd "$nipe_path"
-	nipe_status=$(sudo perl nipe.pl status | grep -oP "(?<=Status: )\b(true|false)\b")
-	[[ $nipe_status == "true" ]] && return 0 #echo "[#] nipe is already running." && return 0
+	cd "$NIPE_PATH"
+	local nipe_status=$(sudo perl nipe.pl status | grep -oP "(?<=Status: )\b(true|false)\b")
+	[[ $nipe_status == "true" ]] && kill $load_msg_pid && echo -e "\r$msg " && return 0 #echo "[#] nipe is already running." && return 0
 
 	sudo perl nipe.pl start 
 	
-	nipe_status=$(sudo perl nipe.pl status | grep -oP "(?<=Status: )\b(true|false)\b")
 
-	[[ $nipe_status == "true" ]] && return 0
+	while [ "$nipe_status" != "true" ]
+	do
+		reset_circuit >/dev/null 2>&1
+		nipe_status=$(sudo perl nipe.pl status | grep -oP "(?<=Status: )\b(true|false)\b")
+	done
+	
+	kill $load_msg_pid
+	echo -e "\r$msg "
 
-	echo "[!] Unexpected Error while starting nipe service"
-
-	think "mmm... maybe try again..."
-
-	sudo perl nipe.pl stop
-	exit 1
+	return 0
 }
 
+# Function to display the current:
+#	1. public IP
+#	2. country
 get_spoofed_value() {
-	#cd "$nipe_path" && sudo perl nipe.pl status || { echo "[*] atempting to reinstall"; sudo perl nipe.pl install; sudo perl nipe.pl status 2>/dev/null || exit 1; }
-	country=$(sudo perl nipe.pl status | xargs geoiplookup)
-	country=$(echo "$country" | cut -d ' ' -f 5-)
-	output=$(sudo perl nipe.pl status)
-	ip=$(echo "$output" | grep -oE '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+')
+	local country=$(sudo perl nipe.pl status | xargs geoiplookup | cut -d ' ' -f 5-)
+	local ip=$(sudo perl nipe.pl status | awk '/Ip:/ {print $NF}')
 
 	echo -e "[*] You are anonymous.. Connecting to the remote Server.\n"
 	echo "[*] Your Spoofed IP address is: $ip, Spoofed country: $country"
 }
 
+# Function to request the user to input the remote server credentials
+# Note:
+#	the password field is hidden in order to protect the user from over the sholder attacks
+#	the port field may be skiped and assumed as the default
+get_remote_creds() {
+	read -p "[?] Enter remote user: " rm_user
+	read -s -p "[?] Enter remote password: " rm_pass; echo
+	read -p "[?] Enter remote address: " rm_ip
+	read -p "[?] Enter remote port: " rm_port; [ -z "$rm_port" ] && rm_port=22
+}
+
+# Function to initiate run a remote scan on the previously defined target 
 remote_scan() {
-	local rm_scans="/home/michael" #TODO change to dynamic
-	local rm_port=22
-	local whois_scan="whois_$target_domain"
-	local nmap_scan="nmap_$target_domain"
+	local readonly whois_scan="whois_$target_domain"
+	local readonly nmap_scan="nmap_$target_domain"
 	local array_string=$(printf "%s " "${rm_programs[@]}")
 
-	sshpass -p $rm_pass ssh -o StrictHostKeyChecking=no $rm_user@$rm_ip "$(declare -f remote_script); $(declare -f install_programs); $(declare -f check_installed); remote_script $target_domain $array_string" 
+	get_remote_creds
+
+	audit "[*] Starting remote scan"
+	sshpass -p $rm_pass ssh -o StrictHostKeyChecking=no $rm_user@$rm_ip "$(declare -f remote_script); $(declare -f install_programs); $(declare -f check_installed); remote_script $target_domain $rm_pass $array_string" >/dev/null 2>&1 
 
 	echo -e "\n[*] Whoising victim's address:"
 	sshpass -p $rm_pass ssh -p $rm_port -o StrictHostKeyChecking=no $rm_user@$rm_ip whois $target_domain > $SCAN_PATH/$whois_scan
 	echo "[@] Whois data was saved into $SCAN_PATH/$whois_scan."
-	# Log Audit 
-	echo "$(date)- [*] whois data collected for: $target_domain" >> $LOG_PATH
+	audit "[*] whois data collected for: $target_domain"
 	
 	echo -e "\n[*] Scanning victims's address:"
 	sshpass -p $rm_pass ssh -p $rm_port -o StrictHostKeyChecking=no $rm_user@$rm_ip nmap -vvv -T4 $target_domain > $SCAN_PATH/$nmap_scan
 	echo "[@] Nmap data was saved into $SCAN_PATH/$nmap_scan."
-	# Log Audit
-	echo "$(date)- [*] Nmap data collected for: $target_domain" >> $LOG_PATH
+	audit "[*] Nmap data collected for: $target_domain"
 }
 
-#####################REMOTE-SCRIPTS####################
-
+# Function that runs on the remote server itself
+# Installes the needed apps
+# Anonymize the connection
+# Displayes the spoofed values
 remote_script() {
 	local programs=("whois" "nmap" "geoip-bin" "git" "curl" "build-essential" "nipe")
-	local sudo_pass="michael"
+	local sudo_pass=$2
 	local target=$1
 
-	echo "$sudo_pass" | sudo -S ls >/dev/null 2>&1
+	echo "$sudo_pass" | sudo -S install_programs ${programs[@]} 
 
-	echo "$sudo_pass" | sudo -S mkdir $log_dir 2>/dev/null
-
-	install_programs ${programs[@]} >/dev/null 2>&1
-
-	sudo perl $nipe_path/nipe.pl start
-	local status=$(sudo perl $nipe_path/nipe.pl status) # TODO handel a failed status
+	sudo perl $NIPE_PATH/nipe.pl start
+	local status=$(sudo perl $NIPE_PATH/nipe.pl status) # TODO handel a failed status
 	
 
 	local remote_ip=$(echo $status | awk '/Ip:/ { print $NF } ')
@@ -250,32 +410,19 @@ remote_script() {
 	echo "Country: $country_ip"
 }
 
-get_whois() {
-	local target=$1
-	local rm_pass=$2
-	local scan_path="whois_$target"
-
-	pwd
-	echo "$rm_pass" | sudo -S touch $scan_path
-	sudo chmod a+w $scan_path
-	whois $target > $scan_path	
-}
-
-#########################################################
-
-# main() {
+# Main function to run the entire script
+main() {
 	init_checks $@
 	
-	install_programs ${programs[@]} </dev/null 2>&1
-	
-	spoof_address
+	check_connectivity
+
+
+	install_programs ${programs[@]}  #>/dev/null 2>&1
+
+	spoof_address #>/dev/null 2>&1 
 	get_spoofed_value
-	
-	declare -g target_domain=""
-	while [ -z "$target_domain" ]
-	do
-		check_domain_format
-	done
+
+	get_target
 	
 	echo -e "\n[*] Connecting to Remote Server:"
 	
@@ -285,12 +432,7 @@ get_whois() {
 	
 	say "Have a good day"
 	
-	revert_to_default
-# }
+	revert_to_default >/dev/null 2>&1
+}
 
-# main "{@}"
-
-# get target for remote
-# nmap -p 22 --exclude 10.0.0.50 10.0.0.50/24 | grep -B 4 "open" |  awk '/Nmap scan/{print $NF}' | head -n 1
-# hydra -l michael -P temp ssh://10.0.0.70 -o rm_creds >/dev/null 2>&1
-
+main "${@}"
